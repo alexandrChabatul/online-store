3cdb3bd7e38c55e0322a5fed443bf3a3f071ca53
Diana_Bobrova <dziana_babrova@epam.com>
2022-12-20 12:33:29 +0300

    fix: fix linter errors

--- src/utils/element-generator.ts
+++ src/utils/element-generator.ts
@@ -1,5 +1,4 @@
 export class NewElement {
-    constructor() {}
 
     static createBaseElement(elementType: string, className: string): HTMLElement {
         const element = document.createElement(elementType);
@@ -23,48 +22,48 @@ export class NewElement {
     }
 
     static createInputRange(className: string): HTMLInputElement {
-      const element = document.createElement('input');
-      element.type = 'range';
-      element.classList.add(className);
-      return element;
+        const element = document.createElement('input');
+        element.type = 'range';
+        element.classList.add(className);
+        return element;
     }
 
     static createInputText(className: string, placeholder: string): HTMLInputElement {
-      const element = document.createElement('input');
-      element.type = 'text';
-      element.placeholder = placeholder;
-      element.classList.add(className);
-      return element;
+        const element = document.createElement('input');
+        element.type = 'text';
+        element.placeholder = placeholder;
+        element.classList.add(className);
+        return element;
     }
 
     static createInputNumber(className: string, value: string): HTMLInputElement {
-      const element = document.createElement('input');
-      element.type = 'number';
-      element.value = value;
-      element.classList.add(className);
-      return element;
+        const element = document.createElement('input');
+        element.type = 'number';
+        element.value = value;
+        element.classList.add(className);
+        return element;
     }
 
     static createCheckbox(className: string, id: string, name: string, value: string): HTMLInputElement {
-      const element = document.createElement('input');
-      element.type = 'checkbox';
-      element.id = id;
-      element.name = name;
-      element.value = value;
-      element.classList.add(className);
-      return element;
+        const element = document.createElement('input');
+        element.type = 'checkbox';
+        element.id = id;
+        element.name = name;
+        element.value = value;
+        element.classList.add(className);
+        return element;
     }
 
     static createSelect(className: string): HTMLSelectElement {
-      const element = document.createElement('select');
-      element.classList.add(className);
-      return element;
+        const element = document.createElement('select');
+        element.classList.add(className);
+        return element;
     }
 
     static createOPtion(value: string, text: string): HTMLOptionElement {
-      const element = document.createElement('option');
-      element.value = value;
-      element.textContent = text;
-      return element;
+        const element = document.createElement('option');
+        element.value = value;
+        element.textContent = text;
+        return element;
     }
 }
