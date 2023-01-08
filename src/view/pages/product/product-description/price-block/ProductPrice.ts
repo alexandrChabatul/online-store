4677db89ad28ee403c421fb9caa90e72ce42c908
Alex Ch <sashkill94@gmail.com>
2023-01-08 19:23:53 +0300

    feat: make product page responsive

--- src/view/pages/product/product-description/price-block/ProductPrice.ts
+++ src/view/pages/product/product-description/price-block/ProductPrice.ts
@@ -6,7 +6,7 @@ class ProductPrice {
     getProductPriceBlock(price: number, currentPrice: number, discount: number) {
         const container = ElementsFactory.createDivElement('price-info');
         const priceBlock = ElementsFactory.createDivElement('price-info__price');
-        priceBlock.innerHTML = `<p class="price-info__prev">${price} ${appConstants.currency}</p><p class="price-info__title">Price: <span class="price-info__real">${currentPrice} ${appConstants.currency}</span></p>`;
+        priceBlock.innerHTML = `<p class="price-info__title">Price: <span class="price-info__prev">${price} ${appConstants.currency}</span></p> <p class="price-info__real">${currentPrice} ${appConstants.currency}</p>`;
         const discountBlock = ElementsFactory.createDivElement('price-info__discount');
         discountBlock.innerHTML = `<p class="price-info__title">Discount:</p><p class="price-info__discount">${discount}%</p>`;
         container.append(priceBlock);
